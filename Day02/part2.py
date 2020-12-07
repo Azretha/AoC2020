de6fe5433506169e54dca7403843eb66e6739803
Azretha <75452917+Azretha@users.noreply.github.com>
2020-12-07 23:30:05 +0100

    fix pt2 to work
    Fixed dumb zero-based index correcting

--- Day02/part2.py
+++ Day02/part2.py
@@ -1,9 +1,5 @@
 import re
 
-##
-## not a working solution yet
-##
-
 def readFile(input):
     passwords = []
     with open(input) as f:
@@ -17,31 +13,21 @@ answer = readFile('input.txt')
 valids = 0
 
 for line in answer:
-    pos1 = int(line[0])+1 #correcting for zero-based indexing
-    pos2 = int(line[1])+1 #same
+    pos1 = int(line[0])-1 #correcting for zero-based indexing
+    pos2 = int(line[1])-1 #same
     c = str(line[2])
     pw = str(line[3])
 
     if c in pw:
-        # if both positions are in index range
         if (pos1 in range(len(pw))) and (pos2 in range(len(pw))):
             if ((pw[pos1] == c and pw[pos2] != c) or (pw[pos1] != c and pw[pos2] == c)):
-                #print(pw)
-                #print(pw[pos1])
-                #rint(pw[pos2])
                 valids += 1               
         elif ((pos1 in range(len(pw))) and (pos2 not in range(len(pw)))):
             if pw[pos1] == c:
-                #print(pw)
-                #print(pw[pos1])
                 valids += 1                
         elif ((pos2 in range(len(pw))) and (pos1 not in range(len(pw)))):
             if pw[pos2] == c:
-                #print(pw)
-                #print(pw[pos2])
                 valids += 1
-        else:
-            pass
 
 print('Total number of valid passwords in file: ' + str(valids))
 
